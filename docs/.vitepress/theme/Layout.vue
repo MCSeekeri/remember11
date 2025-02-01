c180495a8cbdb49da1bff74492337eb0b05194bc
MCSeekeri <mcseekeri@outlook.com>
2025-02-01 11:16:53 +0000

    feat: 评论区剧透提示

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -9,6 +9,9 @@ const { Layout } = DefaultTheme;
 <template>
   <Layout>
     <template #doc-after>
+      <div style="text-align: center; margin-top: 20px; margin-bottom: 20px;">
+        <h1 style="font-size: 24px;">评论区可能含有剧透内容，请通关后观看。</h1>
+      </div>
       <Waline />
     </template>
   </Layout>
