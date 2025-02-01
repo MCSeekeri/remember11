00000050f8f5609501d0df7cfaf5b0f77a436280
MCSeekeri <mcseekeri@outlook.com>
2025-02-01 11:08:39 +0000

    fix: 修复 Waline

--- docs/.vitepress/theme/Waline.vue
+++ docs/.vitepress/theme/Waline.vue
@@ -4,15 +4,21 @@
   </ClientOnly>
 </template>
 <script setup>
-import { Waline } from "@waline/client/component";
-import { computed, onMounted } from "vue";
+import { Waline } from '@waline/client/component';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
-import "@waline/client/waline.css";
+import '@waline/client/style';
 
 const serverURL = "https://r11.comments.mcseekeri.com";
-const dark = 'html[class="dark"]';
+const dark = 'html.dark';
 const turnstileKey = "0x4AAAAAAAgSnJC0FwcTFI6c";
-onMounted(() => {
-  let path = computed(() => window.location.path);
-});
 </script>
+
+<style>
+ html.dark {
+   --waline-bg-color: #1b1b1f !important;
+   --waline-active-color: #a8b1ff !important;
+   --waline-theme-color: #a8b1ff !important;
+  }
+</style>
